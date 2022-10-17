73362245eeba1d6ede37acaf5fd19ad78ddc758b
VanLinh-Flutter Dev <linh39509667@gmail.com>
2022-10-17 14:56:20 +0700

    finish onboarding screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 
+const icGoogle = 'assets/images/ic_google.png';
+const icFacebook = 'assets/images/ic_facebook.png';
+const imgOnBoarDing = 'assets/images/onboarding.png';
+
 void main() {
-  const icGoogle = 'assets/images/ic_google.png';
-  const icFacebook = 'assets/images/ic_facebook.png';
   runApp(
     const MaterialApp(
-      home: DemoScreen(icGoogle: icGoogle, icFacebook: icFacebook),
+      home: DemoScreen(),
     ),
   );
 }
@@ -13,104 +15,141 @@ void main() {
 class DemoScreen extends StatelessWidget {
   const DemoScreen({
     Key? key,
-    required this.icGoogle,
-    required this.icFacebook,
   }) : super(key: key);
 
-  final String icGoogle;
-  final String icFacebook;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.amber,
-      body: Container(
-        child: Column(
-          children: [
-            Container(
-              padding: const EdgeInsets.only(left: 24, top: 13),
-              decoration: BoxDecoration(
-                color: const Color(0xff101010),
-                borderRadius: BorderRadius.circular(100),
+      backgroundColor: const Color(0xffffffff),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 126, bottom: 70),
+                child: Image.asset(imgOnBoarDing, width: 236, height: 196),
               ),
-              child: Text(
-                "Get Started".toUpperCase(),
-                style: const TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w700,
-                  color: Color(0xffFFFFFF),
+              const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 35),
+                child: Text(
+                  "Enjoy The New Arrival Product",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 36),
                 ),
               ),
-            ),
-            Row(
-              children: [
-                Container(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 16, horizontal: 25),
+              const Padding(
+                padding:
+                    EdgeInsets.only(top: 20, bottom: 107, left: 45, right: 45),
+                child: Text(
+                  "Get your dream item easily and safely with Shoesly. and get other interesting offers",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontSize: 14,
+                    color: Color(0xff6F6F6F),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30)
+                    .copyWith(bottom: 20),
+                child: Container(
+                  width: double.infinity,
+                  height: 50,
                   decoration: BoxDecoration(
-                      color: Colors.white,
                       borderRadius: BorderRadius.circular(100),
-                      border: Border.all(color: const Color(0xffE7E7E7))),
-                  child: Row(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: const Color(0xffF3F3F3),
-                        ),
-                        padding: const EdgeInsets.all(10),
-                        child: Image.asset(
-                          icGoogle,
-                          width: 20,
-                          height: 20,
-                        ),
+                      color: const Color(0xff101010)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Text(
+                      "Get Started".toUpperCase(),
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w700,
+                        color: Color(0xffFFFFFF),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 8.0),
-                        child: Text(
-                          "google".toUpperCase(),
-                          style: const TextStyle(
-                              fontWeight: FontWeight.w700, fontSize: 14),
-                        ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30 ),
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 10),
+                  child: Row(
+                    children: const [
+                      Expanded(
+                        child: MyButtonSocial(icon: icGoogle,text: "google",),
+                      ),
+                      SizedBox(width: 15),
+                      Expanded(
+                        child: MyButtonSocial(icon: icFacebook,text: "facebook",),
                       ),
                     ],
                   ),
                 ),
-                Container(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 16, horizontal: 25),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(100),
-                      border: Border.all(color: const Color(0xffE7E7E7))),
-                  child: Row(
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20),
-                          color: const Color(0xffF3F3F3),
-                        ),
-                        padding: const EdgeInsets.all(10),
-                        child: Image.asset(
-                          icFacebook,
-                          width: 20,
-                          height: 20,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 8.0),
-                        child: Text(
-                          "facebook".toUpperCase(),
-                          style: const TextStyle(
-                              fontWeight: FontWeight.w700, fontSize: 14),
-                        ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+}
+
+class MyButtonSocial extends StatelessWidget {
+  const MyButtonSocial({
+    Key? key,
+    required this.icon,
+    required this.text,
+  }) : super(key: key);
+
+  final String icon;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(0),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(100),
+                border: Border.all(color: const Color(0xffE7E7E7))),
+            child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      color: const Color(0xffF3F3F3),
+                    ),
+                    padding: const EdgeInsets.all(10),
+                    child: Image.asset(
+                      icon,
+                      width: 10.0,
+                      height: 10.0,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0),
+                    child: Text(
+                     text.toUpperCase(),
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w700, fontSize: 14),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
